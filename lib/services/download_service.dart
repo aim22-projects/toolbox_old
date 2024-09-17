04e56ef5eaef1063462e38e3606e79ba9fd9d141
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2024-09-17 19:52:09 +0530

    create download service

---
+++ lib/services/download_service.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:toolbox/models/download_task.dart';
+import 'package:http/http.dart' as http;
+
+class DownloadService {
+  DownloadService._internal();
+
+  static DownloadService get _instance => DownloadService._internal();
+
+  factory DownloadService() => _instance;
+
+  Future<void> downloadFile(
+    DownloadTask task,
+    Function(double) onProgress,
+  ) async {
+    var request = http.Request('GET', Uri.parse(task.url));
+
+    final response = await http.Client().send(request);
+
+    if (response.statusCode != 200) throw Exception('Failed to download file');
+
+    final directory = await getApplicationDocumentsDirectory();
+    final filePath = '${directory.path}/${task.name}';
+    final file = File(filePath);
+
+    final totalBytes = response.contentLength ?? 0;
+    int receivedBytes = 0;
+
+    final sink = file.openWrite();
+    response.stream.listen(
+      (List<int> chunk) {
+        receivedBytes += chunk.length;
+        onProgress(receivedBytes / totalBytes * 100);
+        sink.add(chunk);
+      },
+      onDone: () async {
+        await sink.close();
+      },
+      onError: (error) {
+        sink.close();
+        throw error;
+      },
+    );
+  }
+}
