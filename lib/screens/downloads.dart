137de9bf901917d2f2d8936e17a31f97303f1d57
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2024-09-21 18:10:55 +0530

    fix downloads screen

--- lib/screens/downloads.dart
+++ lib/screens/downloads.dart
@@ -23,50 +23,48 @@ class DownloadsScreenContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // TODO: use popscope to handle back gesture
     return Consumer<DownloadTasksProvider>(
-      builder: (context, downloadsProvider, child) => PopScope(
-        // onPopInvokedWithResult: (bool didPop, Object? result) {},
-        child: Scaffold(
-          resizeToAvoidBottomInset: true,
-          appBar: AppBar(title: const Text('Downloads')),
-          body: RefreshIndicator(
-            onRefresh: downloadsProvider.fetchRecords,
-            child: ListView.separated(
-              shrinkWrap: true,
-              physics: const AlwaysScrollableScrollPhysics(),
-              itemBuilder: (context, index) => DownloadTile(
-                downloadTask: downloadsProvider.downloads[index],
-                selected: false,
-                onTap: () => downloadsProvider.openFile(
-                  downloadsProvider.downloads[index],
-                ),
-                onLongPress: () => DownloadDetailsSheet.show(
-                  context,
-                  downloadsProvider.downloads[index],
-                  () async {
-                    await downloadsProvider
-                        .deleteTask(downloadsProvider.downloads[index]);
-                    // ignore: use_build_context_synchronously
-                    GoRouter.of(context).pop();
-                  },
-                ),
+      builder: (context, downloadsProvider, child) => Scaffold(
+        resizeToAvoidBottomInset: true,
+        appBar: AppBar(title: const Text('Downloads')),
+        body: RefreshIndicator(
+          onRefresh: downloadsProvider.fetchRecords,
+          child: ListView.separated(
+            shrinkWrap: true,
+            physics: const AlwaysScrollableScrollPhysics(),
+            itemBuilder: (context, index) => DownloadTile(
+              downloadTask: downloadsProvider.downloads[index],
+              selected: false,
+              onTap: () => downloadsProvider.openFile(
+                downloadsProvider.downloads[index],
               ),
-              separatorBuilder: (context, index) => const Divider(
-                height: 1,
-                indent: 16,
-                endIndent: 16,
+              onLongPress: () => DownloadDetailsSheet.show(
+                context,
+                downloadsProvider.downloads[index],
+                () async {
+                  await downloadsProvider
+                      .deleteTask(downloadsProvider.downloads[index]);
+                  // ignore: use_build_context_synchronously
+                  GoRouter.of(context).pop();
+                },
               ),
-              itemCount: downloadsProvider.downloads.length,
             ),
+            separatorBuilder: (context, index) => const Divider(
+              height: 1,
+              indent: 16,
+              endIndent: 16,
+            ),
+            itemCount: downloadsProvider.downloads.length,
           ),
-          floatingActionButton: FloatingActionButton(
-            // onPressed: downloadsProvider.goToNewDownloadScreen,
-            onPressed: () async {
-              await NewDownloadSheet.show(context, null);
-              await downloadsProvider.fetchRecords();
-            },
-            child: const Icon(Icons.add),
-          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          // onPressed: downloadsProvider.goToNewDownloadScreen,
+          onPressed: () async {
+            await NewDownloadSheet.show(context, null);
+            await downloadsProvider.fetchRecords();
+          },
+          child: const Icon(Icons.add),
         ),
       ),
     );
