170e5b5171f8314b3648230ab8133ebaf91bc948
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2024-09-21 14:18:57 +0530

    fix main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:toolbox/routes.dart';
-import 'package:toolbox/services/download_service.dart';
 import 'package:toolbox/services/notification_service.dart';
 import 'package:toolbox/services/storage_service.dart';
 
@@ -9,7 +8,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   NotificationService.requestPermission();
   StorageService.requestPermission();
-  DownloadService.initializeManager();
   runApp(const MyApp());
 }
 
