23e3abd220adc54872cee90df9141586bafc0457
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2024-10-15 17:40:51 +0530

    create httpex download extension

---
+++ lib/extensions/http_ex.dart
@@ -0,0 +1,60 @@
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:path/path.dart';
+
+class HttpEx {
+  HttpEx._();
+  static download(
+    String url,
+    String path,
+    void Function() onStarted,
+    void Function(int received, int total) onProgress,
+    void Function() onComplete,
+    void Function(Object error) onError,
+  ) async {
+    try {
+      // 1. create http request
+      var request = http.Request('GET', Uri.parse(url));
+      final response = await http.Client().send(request);
+      if (response.statusCode != 200) {
+        throw Exception('Failed to download file');
+      }
+      // 2. capture download size
+      int totalSize = response.contentLength ?? 0;
+      int received = 0;
+      // 3. create download directory if not exists
+      await Directory(dirname(path)).create();
+      // 4. create file
+      final file = File(path);
+      final fileSink = file.openWrite();
+      // call on started callback
+      onStarted();
+      // 5. listen downloading data and write to file
+      response.stream.listen(
+        (List<int> chunk) async {
+          // download sink
+          fileSink.add(chunk);
+          // update progress
+          received += chunk.length;
+          // call in progress callback
+          onProgress(received, totalSize);
+        },
+        onDone: () async {
+          // close file write
+          await fileSink.close();
+          // call on complete callback
+          onComplete();
+        },
+        onError: (error) async {
+          // close file write
+          await fileSink.close();
+          // call on error callback
+          onError(error);
+        },
+      );
+    } catch (ex) {
+      onError(ex);
+    }
+  }
+}
